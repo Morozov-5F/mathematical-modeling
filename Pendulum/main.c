e85406b9f99675cd475fa6df14c946e2863beabe
Evgeniy Morozov <zhe3145@yandex.ru>
2016-09-13 12:56:36 +0300

    [Pendulum] Added options

--- Pendulum/main.c
+++ Pendulum/main.c
@@ -13,7 +13,7 @@
 
 #define DEFAULT_STEP            (1)
 #define MAX_STRING_SIZE         (4096)
-#define OPTIONS                 "a:e:f:hmo:r:t:A:V:"
+#define OPTIONS                 "a:e:f:hmo:r:t:vA:V:"
 
 #define OPTION_MODE_GENERAL     "general"
 #define OPTION_MODE_HARMONIC    "harmonic"
@@ -28,32 +28,49 @@
 #define OPTION_DEFAULT_RERROR   (1e-3)
 
 #define OPTION_DEFAULT_TIMESTEP (1e+0)
+#define OPTION_DEFAULT_END_TIME (1e+1)
 
 typedef int (*system_callback)(double, const double *, double *, void *);
-
-typedef struct option_mode_s
-{
-    char mode_name [MAX_STRING_SIZE];
-
-} option_mode_t;
-
-void print_usage();
 
 int general_case_cb(double t, const double y[], double dydt[], void *params);
 int small_angles_cb(double t, const double y[], double dydt[], void *params);
 int balance_cb(double t, const double y[], double dydt[], void *params);
 
+typedef struct option_mode_s
+{
+    char mode_name[MAX_STRING_SIZE];
+    system_callback mode_callback;
+} option_mode_t;
+
+static option_mode_t option_mode [] =
+{
+    { OPTION_MODE_GENERAL,  general_case_cb },
+    { OPTION_MODE_HARMONIC, small_angles_cb },
+    { OPTION_MODE_EXP,      balance_cb },
+};
+
+void print_usage();
+
 int solve_ode(gsl_odeiv2_system * sys, double y [],double eps_abs,
-              double eps_rel, double time_end);
+              double eps_rel, double time_step, double time_end);
 
 int main(int argc, char *const * argv)
 {
     int retval = GSL_SUCCESS;
+    int i = 0;
+    int found = 0;
     char option = 0;
-    double omega = OPTION_DEFAULT_OMEGA;
-    double eps_rel = OPTION_DEFAULT_RERROR;
-    double eps_abs = OPTION_DEFAULT_AERROR;
-    double y[2] = { OPTION_DEFAULT_ANGLE, OPTION_DEFAULT_VELOCITY };
+    int verbose = 0;
+
+    char file_name[MAX_STRING_SIZE] = OPTION_DEFAULT_FILE;
+
+    double end_time  = OPTION_DEFAULT_END_TIME;
+    double time_step = OPTION_DEFAULT_TIMESTEP;
+    double omega     = OPTION_DEFAULT_OMEGA;
+    double eps_rel   = OPTION_DEFAULT_RERROR;
+    double eps_abs   = OPTION_DEFAULT_AERROR;
+    double y[2]      = { OPTION_DEFAULT_ANGLE, OPTION_DEFAULT_VELOCITY };
+
     system_callback function = general_case_cb;
     gsl_odeiv2_system sys;
 
@@ -64,15 +81,31 @@ int main(int argc, char *const * argv)
             case 'a':
                 if (1 != sscanf(optarg, "%le", &eps_abs))
                 {
-                    printf("Error: bad angle value. Value should be in scientific notation (i.e 1e+2)\n");
+                    fprintf(stderr, "Error: bad absolute error value. Value should be in scientific notation (i.e 1e+2)\n");
                     retval = GSL_ERANGE;
                     goto done;
                 }
             break;
             case 'e':
+                for (i = 0; i < sizeof(option_mode) / sizeof(option_mode_t);
+                    ++i)
+                {
+                    if (!strcmp(optarg, option_mode[i].mode_name))
+                    {
+                        function = option_mode[i].mode_callback;
+                        found = 1;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    fprintf(stderr, "Error: bad mode value.");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
             break;
             case 'f':
-                /* Set output file */
+                strcpy(file_name, optarg);
             break;
             case 'h':
                 print_usage();
@@ -83,19 +116,55 @@ int main(int argc, char *const * argv)
                 /* Implemet manual mode */
             break;
             case 'o':
-                /* Set omega parameter */
+                if (1 != sscanf(optarg, "%le", &omega))
+                {
+                    fprintf(stderr, "Error: bad omega parameter value. Value should be in scientific notation (i.e 1e+2)\n");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
             break;
             case 'r':
-                /* Set relative error */
+                if (1 != sscanf(optarg, "%le", &eps_rel))
+                {
+                    fprintf(stderr, "Error: bad relative error value. Value should be in scientific notation (i.e 1e+2)\n");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
             break;
             case 't':
-                /* Set timestamp */
+                if (1 != sscanf(optarg, "%le", &time_step))
+                {
+                    fprintf(stderr, "Error: bad time step value. Value should be in scientific notation (i.e 1e+2)\n");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
+            break;
+            case 'v':
+                verbose = 1;
             break;
             case 'A':
-                /* Set initial angle */
+                if (1 != sscanf(optarg, "%le", &y[0]))
+                {
+                    fprintf(stderr, "Error: bad initial angle value. Value should be in scientific notation (i.e 1e+2)\n");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
+            break;
+            case 'T':
+                if (1 != sscanf(optarg, "%le", &end_time))
+                {
+                    fprintf(stderr, "Error: bad end time value. Value should be in scientific notation (i.e 1e+2)\n");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
             break;
             case 'V':
-                /* Set initial velocity */
+                if (1 != sscanf(optarg, "%le", &y[1]))
+                {
+                    fprintf(stderr, "Error: bad initial velocity value. Value should be in scientific notation (i.e 1e+2)\n");
+                    retval = GSL_ERANGE;
+                    goto done;
+                }
             break;
             default:
                 print_usage();
@@ -104,34 +173,59 @@ int main(int argc, char *const * argv)
         }
     }
 
+    if (verbose)
+    {
+        printf("# Absolute error:     %e\n", eps_abs);
+        printf("# Relative error:     %e\n", eps_rel);
+        printf("# Initial angle:      %e\n", y[0]);
+        printf("# Initial velocity:   %e\n", y[1]);
+        for (i = 0; i < sizeof(option_mode) / sizeof(option_mode_t); ++i)
+        {
+            if (function == option_mode[i].mode_callback)
+            {
+                printf("# Mode name:          %s\n", option_mode[i].mode_name);
+            }
+        }
+        printf("# Time step:          %e\n", time_step);
+        printf("# End time:           %f\n", end_time);
+        printf("# File name:          %s\n", file_name);
+    }
+
+    if (stdout != freopen(file_name, "w", stdout))
+    {
+        fprintf(stderr, "Error: could not open file %s", file_name);
+        retval = GSL_FAILURE;
+        goto done;
+    }
+
     sys.function = function;
     sys.jacobian = NULL;
     sys.dimension = 2;
     sys.params = &omega;
 
-    retval = solve_ode(&sys, y, eps_abs, eps_rel, 10);
+    retval = solve_ode(&sys, y, eps_abs, eps_rel, time_step, end_time);
 done:
     return retval;
 }
 
 int solve_ode(gsl_odeiv2_system * sys, double y[], double eps_abs,
-              double eps_rel, double time_end)
+              double eps_rel, double time_step, double time_end)
 {
     int retval = GSL_SUCCESS;
     double t = 0;
-    size_t i;
+    size_t i, n;
     gsl_odeiv2_driver * driver;
 
     driver = gsl_odeiv2_driver_alloc_y_new(sys, gsl_odeiv2_step_rk4,
                                            DEFAULT_STEP, eps_abs, eps_rel);
-
+    n = ceil(time_end / time_step);
     printf("%.5e %.5e %.5e\n", t, y[0], y[1]);
-    for (i = 0; i < time_end; ++i)
+    for (i = 0; i <= n; ++i)
     {
-        retval = gsl_odeiv2_driver_apply(driver, &t, t + 1, y);
+        retval = gsl_odeiv2_driver_apply(driver, &t, i * time_step, y);
         if (retval != GSL_SUCCESS)
         {
-            printf("error: driver returned %d\n", retval);
+            fprintf(stderr, "Error: driver returned %d\n", retval);
             break;
         }
         printf("%.5e %.5e %.5e\n", t, y[0], y[1]);
@@ -189,8 +283,9 @@ void print_usage()
     printf("  -m             Manual mode (not implemented yet)\n");
     printf("  -o <omega>     Omega parameter for system (angular frequency). Default is %e\n", OPTION_DEFAULT_OMEGA);
     printf("  -r <error>     Relative error. Default is %e\n", OPTION_DEFAULT_RERROR);
-    printf("  -t <timestep>  Time step. Default is %e\n", OPTION_DEFAULT_TIMESTEP);
+    printf("  -t <time_step> Time step. Default is %e\n", OPTION_DEFAULT_TIMESTEP);
     printf("  -v             Verbose mode\n");
     printf("  -A <angle>     Initial angle (in radians). Default is %e\n", OPTION_DEFAULT_ANGLE);
+    printf("  -T <time>      End time. Default is %e\n", OPTION_DEFAULT_END_TIME);
     printf("  -V <velocity>  Initial velocity (in radians per second). Default is %e\n", OPTION_DEFAULT_VELOCITY);
 }
--- Pendulum/plot.gp
+++ Pendulum/plot.gp
@@ -3,12 +3,12 @@ set term postscript
 set out "plot.ps"
 
 set grid
-set autoscale xy
+# set autoscalee xy
 
 set title "Pendulum oscillations"
 
-set xlabel "Time"
-set ylabel "Amplitude, radians"
+set xlabel  "Time"
+set ylabel  "Amplitude, radians"
 set y2label "Velocity, radians per second"
 
 set key out
@@ -17,5 +17,7 @@ set ytics nomirror
 set y2tics
 set tics out
 
-plot "data.dat" using 1:2:(1e-2) title 'Angle' axes x1y1 with errorbars,\
+plot "data.dat" using 1:2 title 'Angle' axes x1y1 w l,\
 	"data.dat" using 1:3 title 'Velocity' axes x1y2 w l
+
+plot "data.dat" using 2:3 title 'Phase portrait' axes x1y1 w l
